f45ff787138c9413d6b6a0ecaf1f283ce95e684b
paraggupta1993 <paraggupta1993@gmail.com>
2013-04-27 13:51:10 +0530

    still db trying

--- db/migrate/20130427075710_alter_db.rb
+++ db/migrate/20130427075710_alter_db.rb
@@ -1,8 +1,8 @@
 class AlterDb < ActiveRecord::Migration
   def up
-    add_column :name, :string
-    add_column :author, :string
-    add_column :status, :boolean
+    add_column :books, :name, :string
+    add_column :books, :author, :string
+    add_column :books , :status, :boolean
     
   end
 
